2e056cef768fc973dad7b718a5d81f3fb7dd8c77
Kath <groosgonzalezkatherin@gmail.com>
2024-06-26 09:47:13 -0400

    Update ajax_sample.js

--- Ajax_sample/javascripts/ajax_sample.js
+++ Ajax_sample/javascripts/ajax_sample.js
@@ -1,42 +1,41 @@
 let number = 0;
+let data = [];
 const videoArea = document.getElementById("video");
 const titleArea = document.getElementById("title");
 const contentArea = document.getElementById("content");
 const button = document.getElementById('btn');
 
-function getData() {
-    button.addEventListener('click', e => {
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    titleArea.innerHTML = request.response[number].title;
-                    contentArea.innerHTML = request.response[number].content;
-                    videoArea.setAttribute("src", request.response[number].url);
-                    number == 2 ? number = 0 : number++;
-                }
-            }
+function fetchData(callback) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            data= request.response;
+            callback();
         }
-        request.open("GET", "ajax.json");
-        request.responseType = "json";
-        request.send(null);
-    })
-    function changeVideo() {
-        if (data.length === 0) {
-            fetchData(() => updateContent());
-        } else {
-            updateContent();
-        }
-    }
+    };
+    request.open("GET", "../ajax.json");
+    request.responseType = "json";
+    request.send(null);
+}
 
-    function updateContent() {
-        titleArea.innerHTML = data[number].title;
-        contentArea.innerHTML = data[number].content;
-        videoArea.setAttribute("src", data[number].url);
-        number = (number + 1) % data.length;
+function changeVideo() {
+    if (data.length ===0) {
+        fetchData(() => updateContent());
+    } else {
+        updateContent();
     }
+}
 
-    button.addEventListener('click', changeVideo);
+function updateContent() {
+    titleArea.innerHTML = data[number].title;
+    contentArea.innerHTML = data[number].content;
+    videoArea.setAttribute("src", data[number].url);
+    number = (number + 1) % data.length;
+}
+
+button.addEventListener('click', changeVideo);
+
+window.onload = changeVideo;
 
     window.onload = changeVideo;
 }
